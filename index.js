ce6d436474d10e45e9362ca4c1648f2a4cf302f8
davidamankwah <81107673+davidamankwah@users.noreply.github.com>
2023-11-07 20:06:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function calculateFactorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return number * calculateFactorial(number - 1);
+  }
+}
+
+// Example: Calculate the factorial of 5
+const result = calculateFactorial(5);
+console.log("Factorial of 5:", result);
 
